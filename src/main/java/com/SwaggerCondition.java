a8304f8712e17c077249d3ac45bf64b5921b7e67
chris <15732677863@163.com>
2019-12-19 20:44:09 +0800

    commit swagger-spring-boot-autoconfigure-Chris-2019-12-19

---
+++ src/main/java/com/SwaggerCondition.java
@@ -0,0 +1,24 @@
+package com;
+
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+import org.springframework.util.StringUtils;
+
+public class SwaggerCondition implements Condition {
+    @Override
+    public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+        String title = context.getEnvironment().getProperty("swagger.title");
+        String description = context.getEnvironment().getProperty("swagger.description");
+        String basePackage = context.getEnvironment().getProperty("swagger.base-package");
+        if (StringUtils.isEmpty(title)) {
+            throw new RuntimeException("Lack of swagger configuration:swagger.title");
+        }else if(StringUtils.isEmpty(description)){
+            throw new RuntimeException("Lack of swagger configuration:swagger.description");
+        }else if(StringUtils.isEmpty(basePackage)){
+            throw new RuntimeException("Lack of swagger configuration:swagger.base-package");
+        }else {
+            return true;
+        }
+    }
+}
